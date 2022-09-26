1709b765acf66f9d54739300ace8cd856db65af1
mnks90 <108756538+mnks90@users.noreply.github.com>
2022-09-26 21:08:05 +0200

    Update vessel.m

--- DynamicModel/vessel.m
+++ DynamicModel/vessel.m
@@ -4,23 +4,23 @@
 % vessel dynamics can be changed by altering variables in the setup part
 
 % setup
-L1 = 1;                     % vessel length
-L2 = 0.4;                   % vessel width
-L3 = 0.3;                   % thruster spacing
-L4 = 0.4;                   % back-com dist
-L5 = sqrt(L4^2+(L3/2)^2);   % back thruster com dist
-L6 = 0.4;                   % side thruster com dist
-m = 60;                     % mass
-I = 25;                     % inertia
-B = atand((L3/2)/L4);       % torque angle (calculated)
-vxm = 5;                    % max forward speed
-vym = 0.1;                  % max sideways speed
-wlim = 5000;                % max back thruster rpm
-wlm = 2*pi*wlim;            % max back thruster rad/s [do not change]
-wrm = 2*pi*wlim;            % max back thruster rad/s [do not change]
-wtlim = 6000;               % max side thruster rpm
-wtm = 2*pi*wtlim;           % max side thruster rad/s [do not change]
-Rm = 1;                     % max turning radius (back thrusters only)
+L1 = 1;                     % vessel length [m]
+L2 = 0.4;                   % vessel width [m]
+L3 = 0.3;                   % thruster spacing [m]
+L4 = 0.4;                   % back-com dist [m]
+L5 = sqrt(L4^2+(L3/2)^2);   % back thruster com dist [m]
+L6 = 0.4;                   % side thruster com dist [m]
+m = 60;                     % mass [kg]
+I = 25;                     % inertia [kg/m^2]
+B = atand((L3/2)/L4);       % torque angle (calculated) [deg]
+vxm = 5;                    % max forward speed [m/s]
+vym = 0.1;                  % max sideways speed [m/s]
+wlim = 5000;                % max back thruster [rpm]
+wlm = 2*pi*wlim;            % max back thruster [rad/s] [do not change]
+wrm = 2*pi*wlim;            % max back thruster [rad/s] [do not change]
+wtlim = 6000;               % max side thruster [rpm]
+wtm = 2*pi*wtlim;           % max side thruster [rad/s] [do not change]
+Rm = 1;                     % max turning radius [m] (back thrusters only)
 accel = inf;                % simulink will limit dynamics
 
 % find w max for back and side thruster
@@ -35,7 +35,7 @@ eq3 = bf == bvx*(m*vxm)/(wlm^2+wrm^2);
 eq4 = bt == bvy*(m*vym)/(wtm^2);
 
 % one of the variables need to be selected, the rest is calculated
-eq5 = bvy == 80;
+eq5 = bvx == 1.5;
 
 svar = vpasolve([eq1,eq2,eq3,eq4,eq5],[bf,bt,bw,bvx,bvy])
 bf = double(svar.bf);
